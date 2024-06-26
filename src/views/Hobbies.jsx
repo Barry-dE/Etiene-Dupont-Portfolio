ba46f3f9add0d5e85592af4ce714347d2ae16be2
Barigbue <barigbuenbira@gmail.com>
2024-06-26 23:47:01 +0100

    build hobbies markup

---
+++ src/views/Hobbies.jsx
@@ -0,0 +1,61 @@
+const hobbiesText =
+  "His keen eyes for details extends to digital art.I also have a flair for writing, often penning thought-provoking articles and blogs that inspire and engage. His diverse skill set and artistic vision make him a true creative force, dedicated  to exploring and expressing the beauty in every medium";
+
+function Hobbies() {
+  return (
+    <section className="hobbies">
+      <div className="hoobies__wrapper">
+        <div className="hobbies__pane hobbies__pane--large hobbies__pane--1">
+          <div className="hobbies__pane__circles">
+            <span></span>
+            <span></span>
+          </div>
+          <div className="hobbies__media__wrapper">
+            <figure className="hobbies__media">
+              <img src="" alt="" className="hobbies_media__image" />
+            </figure>
+          </div>
+          <div className="corner"></div>
+        </div>
+        <div className="hobbies__pane hobbies__pane--large hobbies__pane--2">
+          <div className="hobbies__pane__circles">
+            <span></span>
+            <span></span>
+          </div>
+          <div className="hobbies__media__wrapper">
+            <figure className="hobbies__media">
+              <img src="" alt="" className="hobbies_media__image" />
+            </figure>
+          </div>
+          <div className="corner"></div>
+        </div>
+        <div className="hobbies__pane hobbies__pane--large  hobbies__pane--3">
+          <div className="hobbies__pane__circles">
+            <span></span>
+            <span></span>
+          </div>
+          <div className="hobbies__media__wrapper">
+            <figure className="hobbies__media">
+              <img src="" alt="" className="hobbies_media__image" />
+            </figure>
+          </div>
+          <div className="corner"></div>
+        </div>
+        <div className="hobbies__pane hobbies__pane--large  hobbies__pane--4">
+          <div className="hobbies__pane__circles">
+            <span></span>
+            <span></span>
+          </div>
+          <div className="hobbies__media__wrapper">
+            <figure className="hobbies__media">
+              <img src="" alt="" className="hobbies_media__image" />
+            </figure>
+          </div>
+          <div className="corner"></div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hobbies;
